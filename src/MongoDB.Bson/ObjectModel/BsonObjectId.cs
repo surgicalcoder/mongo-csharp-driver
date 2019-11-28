ce6331a19dd2c600308a5f45cf52dfd9174aaed7
DmitryLukyanov <dmitry.lukyanov@mongodb.com>
2019-11-28 15:27:53 +0300

    CSHARP-2340: Minor comments fix.

--- src/MongoDB.Bson/ObjectModel/BsonObjectId.cs
+++ src/MongoDB.Bson/ObjectModel/BsonObjectId.cs
@@ -71,7 +71,7 @@ namespace MongoDB.Bson
         /// <param name="machine">The machine hash.</param>
         /// <param name="pid">The PID.</param>
         /// <param name="increment">The increment.</param>
-        [Obsolete("Use new BsonObjectId(new ObjectId(int timestamp, int machine, short pid, int increment)) instead.")]
+        [Obsolete("Use new BsonObjectId(new ObjectId(uint timestamp, int machine, short pid, int increment)) instead.")]
 #pragma warning disable CS3001
         public BsonObjectId(uint timestamp, int machine, short pid, int increment)
 #pragma warning restore CS3001
@@ -247,7 +247,7 @@ namespace MongoDB.Bson
         /// </summary>
         /// <param name="timestamp">The timestamp component.</param>
         /// <returns>A BsonObjectId.</returns>
-        [Obsolete("Use new BsonObjectId(ObjectId.GenerateNewId(int timestamp)) instead.")]
+        [Obsolete("Use new BsonObjectId(ObjectId.GenerateNewId(uint timestamp)) instead.")]
 #pragma warning disable CS3001
         public static BsonObjectId GenerateNewId(uint timestamp)
 #pragma warning restore CS3001
