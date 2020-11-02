305e48c745fae9f27b645749d8b5ef1aac00cf5c
DmitryLukyanov <dmitry.lukyanov@mongodb.com>
2020-11-02 16:52:42 +0300

    CSHARP-2984: Applied Robert's suggestion.

--- tests/MongoDB.Bson.Tests/Serialization/SerializationTests.cs
+++ tests/MongoDB.Bson.Tests/Serialization/SerializationTests.cs
@@ -22,29 +22,36 @@ using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson.TestHelpers;
 using Xunit;
 
-namespace MongoDB.Bson.Tests.Jira
+namespace MongoDB.Bson.Tests.Serialization
 {
-    public class CSharp1559Tests
+    public class SerializationTests
     {
         [Theory]
-        [InlineData(typeof(DerivedWithoutSetter_BaseWithoutSetter), new[] { 1, 2 })]
-        [InlineData(typeof(DerivedWithoutSetter_BaseWithPrivateSetterAndWithProtectedConstructor), new[] { 1, 2 })]
-        [InlineData(typeof(DerivedWithoutSetterAndWithBsonElement_BaseWithoutSetterAndWithProtectedConstructor), new[] { 1, 2 })]
-        [InlineData(typeof(DerivedWithoutSetterAndWithBsonElementAndWithPrivateConstructor_BaseWithoutSetter), new[] { 1, 2 })]
-        [InlineData(typeof(DerivedWithoutSetterAndWithoutBsonElement_AbstractBaseWithoutSetterAndWithProtectedConstructor), new[] { 1, 2 })]
-        [InlineData(typeof(DerivedWithoutSetterAndWithoutBsonElement_AbstractBaseWithoutSetter), new[] { 1, 2 })]
-        [InlineData(typeof(DerivedWithPrivateSetterAndWithBsonElement_BaseWithoutSetter), new[] { 1, 2 })]
-        [InlineData(typeof(DerivedImmutableWithMorePropertiesThanInConstructorAndWithBsonConstructorAttribute_AbstractBaseImmutable), new[] { 2 })]
-        [InlineData(typeof(DerivedImmutableWithMorePropertiesThanInConstructorButWithBsonElementAttributeAndWithBsonConstructorAttribute_AbstractBaseImmutable), new[] { 2 })]
-        [InlineData(typeof(DerivedImmutableWithMorePropertiesThanInConstructorAndWithBsonConstructorAttribute_AbstractBaseImmutableWithConstructor), new[] { 1, 2})]
-        [InlineData(typeof(DerivedImmutableWithMorePropertiesThanInConstructorButWithBsonElementAttributeAndWithBsonConstructorAttribute_AbstractBaseImmutableWithConstructor), new[] { 1, 2 })]
-        [InlineData(typeof(DerivedImmutableWithMorePropertiesThanInConstructorButWithBsonIgnoreAttribute_AbstractBaseImmutableWithAbstractProperty), new[] { 2 })]
-        [InlineData(typeof(DerivedImmutableWithMorePropertiesThanInConstructorButWithBsonIgnoreAttribute_AbstractBaseImmutableWithConstructor), new[] { 1 })]
-        public void Serialization_should_return_expected_result(Type testCaseType, int[] arguments)
+        [InlineData(typeof(DerivedWithoutSetter_BaseWithoutSetter), new[] { 1, 2 }, "{ \"X\" : 1, \"Y\" : 2 }")]
+        [InlineData(typeof(DerivedWithoutSetter_BaseWithPrivateSetterAndWithProtectedConstructor), new[] { 1, 2 }, "{ \"X\" : 1, \"Y\" : 2 }")]
+        [InlineData(typeof(DerivedWithoutSetterAndWithBsonElement_BaseWithoutSetterAndWithProtectedConstructor), new[] { 1, 2 }, "{ \"X\" : 1, \"y\" : 2 }")]
+        [InlineData(typeof(DerivedWithoutSetterAndWithBsonElementAndWithPrivateConstructor_BaseWithoutSetter), new[] { 1, 2 }, "{ \"X\" : 1, \"y\" : 2 }")]
+        [InlineData(typeof(DerivedWithoutSetterAndWithoutBsonElement_AbstractBaseWithoutSetterAndWithProtectedConstructor), new[] { 1, 2 }, "{ \"X\" : 1, \"Y\" : 2 }")]
+        [InlineData(typeof(DerivedWithoutSetterAndWithoutBsonElement_AbstractBaseWithoutSetter), new[] { 1, 2 }, "{ \"X\" : 1, \"Y\" : 2 }")]
+        [InlineData(typeof(DerivedWithPrivateSetterAndWithBsonElement_BaseWithoutSetter), new[] { 1, 2 }, "{ \"X\" : 1, \"y\" : 2 }")]
+        [InlineData(typeof(DerivedImmutableWithMorePropertiesThanInConstructorAndWithBsonConstructorAttribute_AbstractBaseImmutable), new[] { 2 }, "{ \"X\" : 1, \"Y\" : 2 }")]
+        [InlineData(typeof(DerivedImmutableWithMorePropertiesThanInConstructorAndWithBsonConstructorAttribute_AbstractBaseImmutableWithConstructor), new[] { 1, 2 }, "{ \"X\" : 1, \"Y\" : 2  }")]
+        [InlineData(typeof(DerivedImmutableWithMorePropertiesThanInConstructorButWithBsonElementAttributeAndWithBsonConstructorAttribute_AbstractBaseImmutable), new[] { 2 }, "{ \"X\" : 1, \"Y\" : 2 }")]
+        [InlineData(typeof(DerivedImmutableWithMorePropertiesThanInConstructorButWithBsonElementAttributeAndWithBsonConstructorAttribute_AbstractBaseImmutableWithConstructor), new[] { 1, 2 }, "{ \"X\" : 1, \"Y\" : 2 }")]
+        [InlineData(typeof(DerivedImmutableWithMorePropertiesThanInConstructorButWithBsonIgnoreAttribute_AbstractBaseImmutableWithAbstractProperty), new[] { 2 }, "{ \"Y\" : 2 }")]
+        [InlineData(typeof(DerivedImmutableWithMorePropertiesThanInConstructorButWithBsonIgnoreAttribute_AbstractBaseImmutableWithConstructor), new[] { 1 }, "{ \"X\" : 1 }")]
+        [InlineData(typeof(ClassWithTwoConstructorsWhereTheFirstIfNotFullyMatchedAndTheSecondIsFullyMatched), new[] { 1, 2 }, "{ \"X\" : 1, \"Y\" : 2 }")]
+        [InlineData(typeof(ClassWithOneConstructorThatIsNotFullyMatched), new[] { 1, 2 }, "{ \"X\" : 1, \"Y\" : 2, \"Z\" : null }")]
+        public void Serialization_should_return_expected_result(Type testCaseType, int[] arguments, string expectedJson)
         {
             var testCase = Activator.CreateInstance(testCaseType, arguments.Select(a => (object)a).ToArray());
 
             var json = testCase.ToJson();
+            var actualBsonDocument = BsonDocument.Parse(json);
+            actualBsonDocument["_t"].ToString().Should().Be(testCaseType.Name);
+            actualBsonDocument.Remove("_t");
+            actualBsonDocument.Should().Be(BsonDocument.Parse(expectedJson));
+
             var result = BsonSerializer.Deserialize(json, testCaseType);
 
             var x = GetPropertyValue(result, "X");
@@ -61,7 +68,7 @@ namespace MongoDB.Bson.Tests.Jira
             var exception = Record.Exception(() => testCase.ToJson());
             var e = exception.Should().BeOfType<BsonSerializationException>().Subject;
 
-            e.Message.Should().Be("Creator map for class MongoDB.Bson.Tests.Jira.CSharp1559Tests+DerivedWithMismatchedXTypeComparingWithBase has 2 arguments, but none are configured.");
+            e.Message.Should().Be("Creator map for class MongoDB.Bson.Tests.Serialization.SerializationTests+DerivedWithMismatchedXTypeComparingWithBase has 2 arguments, but none are configured.");
         }
 
         [Fact]
@@ -331,6 +338,36 @@ namespace MongoDB.Bson.Tests.Jira
             public override int X { get; } = 1;
 
             public int Y { get; } = 0; // should be overwritten
+        }
+
+        public class ClassWithTwoConstructorsWhereTheFirstIfNotFullyMatchedAndTheSecondIsFullyMatched
+        {
+            public int? X { get; }
+            public int? Y { get; }
+
+            public ClassWithTwoConstructorsWhereTheFirstIfNotFullyMatchedAndTheSecondIsFullyMatched(int? x)
+                : this(x, null)
+            {
+            }
+
+            public ClassWithTwoConstructorsWhereTheFirstIfNotFullyMatchedAndTheSecondIsFullyMatched(int? x, int? y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+
+        public class ClassWithOneConstructorThatIsNotFullyMatched
+        {
+            public int? X { get; }
+            public int? Y { get; }
+            public int? Z { get; }
+
+            public ClassWithOneConstructorThatIsNotFullyMatched(int? x, int? y)
+            {
+                X = x;
+                Y = y;
+            }
         }
     }
 }
